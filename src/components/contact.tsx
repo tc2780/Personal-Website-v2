bf474a08b240ddce5cdf5f6e598fca508ae7b837
tc2780 <tracyc2780@gmail.com>
2024-03-20 22:21:38 -0700

    testing contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,5 +1,8 @@
 'use client'
+import { GithubOutlined, LinkedinFilled, LinkedinOutlined } from "@ant-design/icons";
+import { Col, Row } from "antd";
 import React from "react";
+import { Fade } from "react-awesome-reveal";
 import styled from 'styled-components';
 
 
@@ -30,24 +33,88 @@ const ContactTitle = styled.div`
                 10px 10px 0px 20px #542a93;
     border-radius: 3px;
     transform: translate(-50px, 30px);
+    // transform: translate(-50px, 30px);
     // transform: translate(-20px, 50px);
 `;
 
 const ContactContent = styled.div`
-    width: fit-content;
+    // width: fit-content;
     padding-top: 20px;
 
     background-color: #3e207f;
     box-shadow: 0px 0px 0px 30px #3e207f,
                 20px 20px 0px 30px #270965;
     border-radius: 3px;
+    // transform: translate(0, -30px);
+`;
+
+const Link = styled.div`
+    font-size: 30px;
+`;
+
+const SubTitle = styled.div`
+    font-size: 25px;
+    font-style: italic;
+    margin-bottom: 10px;
+    color: ghostwhite;
 `;
 
 const ContactMe: React.FC<{}> = () => {
     return (
         <StyledContact id="contactSection">
-            <ContactTitle>contact me</ContactTitle>
-            <ContactContent>TESTING</ContactContent>
+            <Fade cascade damping={0.05} style={{zIndex:"9999", position: "relative"}}>
+                <ContactTitle>contact me</ContactTitle>
+            </Fade>
+            <br />
+            <br />
+            <br />
+            <ContactContent>
+                <Row>
+                    <Col>
+                        <SubTitle>
+                            Please feel free to contact me
+                        </SubTitle>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Link>
+                            <a href="http://github.com/tc2780" target="_blank">
+                                <GithubOutlined style={{paddingRight: "10px"}}/>
+                                GitHub
+                            </a>
+                        </Link>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Link>
+                            <a href="https://www.linkedin.com/in/tracy-chow-6bab40189/" target="_blank">
+                                <LinkedinFilled style={{paddingRight: "10px"}}/>
+                                LinkedIn
+                            </a>
+                        </Link>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <form name="tesitng" method="POST" data-netlify="true">
+                        <p>
+                            <label>Your Name: <input type="text" name="name" /></label>
+                        </p>
+                        <p>
+                            <label>Your Email: <input type="email" name="email" /></label>
+                        </p>
+                        <p>
+                            <label>Message: <textarea name="message"></textarea></label>
+                        </p>
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
+                        </form>
+                    </Col>
+                </Row>
+            </ContactContent>
         </StyledContact>
     )
 }
--- src/components/creativeWorks.tsx
+++ src/components/creativeWorks.tsx
@@ -90,19 +90,22 @@ const Paragraph = styled.div`
 const CreativeWorks: React.FC<{}> = () => {
     return (
         <StyledCreativeWorks id="creativeSection">
-            <Fade fraction={0.1}cascade damping={0.05} style={{zIndex:"9999", position: "relative"}}>
+            <Fade cascade damping={0.05} style={{zIndex:"9999", position: "relative"}}>
                 <CreativeTitle>creative works</CreativeTitle>
             </Fade>
             <br />
             <br />
+            <Fade cascade damping={0.05} >
             <CreativeContent>
-                <SubTitle>{`artwork`}</SubTitle>
-                <Paragraph>{`
-                    These are some artworks I've created. When I was younger, I was mostly a traditional artist, using pencils and markers.
-                    I started to pick up digital drawing more once I found that I had easier access to it most of the time.
-                    Below are some of the digital drawings I've done in the past few years.
-                `}
-                </Paragraph>
+                <Fade fraction={0.1} cascade damping={0.05} >
+                    <SubTitle>{`artwork`}</SubTitle>
+                    <Paragraph>{`
+                        These are some artworks I've created. When I was younger, I was mostly a traditional artist, using pencils and markers.
+                        I started to pick up digital drawing more once I found that I had easier access to it most of the time.
+                        Below are some of the digital drawings I've done in the past few years.
+                    `}
+                    </Paragraph>
+                </Fade>
                 <br />
                 <Image.PreviewGroup
                     preview={{
@@ -113,59 +116,66 @@ const CreativeWorks: React.FC<{}> = () => {
                     <ImageList 
                         // variant="quilted" 
                         cols={3} gap={12}>
-                        <ImageListItem key={"tst"}>
-                            <Image src="./images/catHeader.png" alt="catHeader"/>
-                            <ImageListItemBar position="below" 
-                                title={"tranquil cat"} 
-                                subtitle={"used as the header for this site!"} />
-                        </ImageListItem>
-                        <ImageListItem key={"tst"}>
-                            <Image src="./images/caveGirl.jpeg" alt="caveGirl"/>
-                            <ImageListItemBar position="below"
-                             title={"girl in a cave"} 
-                            //  subtitle={"used as the header for this site!"} 
-                        />
-                        </ImageListItem>
-                        <ImageListItem key={"tst"}>
-                            <Image src="./images/sleepyCat.jpg" alt="sleepyCat"/>
+                        <Fade cascade damping={0.05} >
+                            <ImageListItem key={"tst"}>
+                                <Image src="./images/catHeader.png" alt="catHeader"/>
+                                <ImageListItemBar position="below" 
+                                    title={"tranquil cat"} 
+                                    subtitle={"used as the header for this site!"} />
+                            </ImageListItem>
+                            <ImageListItem key={"tst"}>
+                                <Image src="./images/caveGirl.jpeg" alt="caveGirl"/>
                                 <ImageListItemBar position="below"
-                                title={"sleepy cat"} 
+                                title={"girl in a cave"} 
                                 //  subtitle={"used as the header for this site!"} 
                             />
-                        </ImageListItem>
-                        <ImageListItem key={"tst"}>
-                            <Image src="./images/hawaii.jpg" alt="hawaii"/>
-                            <ImageListItemBar position="below"
-                             title={"hawaii"} 
-                            //  subtitle={"used as the header for this site!"} 
-                            />
-                        </ImageListItem>
-                         
-                        {/*<ImageListItem key={"tst"}>
-                            <img src="./images/AdventureGame.png"/>
-                        </ImageListItem>
-                        <ImageListItem key={"tst"}>
-                            <img src="./images/AdventureGame.png"/>
-                        </ImageListItem>
-                        <ImageListItem key={"tst"}>
-                            <img src="./images/AdventureGame.png"/>
-                        </ImageListItem> */}
+                            </ImageListItem>
+                            <ImageListItem key={"tst"}>
+                                <Image src="./images/sleepyCat.jpg" alt="sleepyCat"/>
+                                    <ImageListItemBar position="below"
+                                    title={"sleepy cat"} 
+                                    //  subtitle={"used as the header for this site!"} 
+                                />
+                            </ImageListItem>
+                            <ImageListItem key={"tst"}>
+                                <Image src="./images/hawaii.jpg" alt="hawaii"/>
+                                <ImageListItemBar position="below"
+                                title={"hawaii"} 
+                                //  subtitle={"used as the header for this site!"} 
+                                />
+                            </ImageListItem>
+                            
+                            {/*<ImageListItem key={"tst"}>
+                                <img src="./images/AdventureGame.png"/>
+                            </ImageListItem>
+                            <ImageListItem key={"tst"}>
+                                <img src="./images/AdventureGame.png"/>
+                            </ImageListItem>
+                            <ImageListItem key={"tst"}>
+                                <img src="./images/AdventureGame.png"/>
+                            </ImageListItem> */}
+                        </Fade>
                     </ImageList>
                     </Image.PreviewGroup>
                 <br />
+                <Fade cascade fraction={0.1} damping={0.05} >
                 <SubTitle>{`animations`}</SubTitle>
                 <Paragraph>{`
                     I have some (very) short animations I've done in the past few years. One I did as a capstone project in my last year of high school, 
                     and also includes some of the concept art I had drawn up for it. The other is technically also considered a technical project
                     as I implemented it with Three.js and OpenGL for an assignment. But I wanted to bring focus to the animations I 
                     did for it and decided to put it in this section as well.
-                `}
+                `}</Paragraph>
                 <br /><br />
                 <SectionTitle>cat animation</SectionTitle>
-                <ImageList variant="masonry" cols={3} gap={12}>
-                        
+                </Fade>
+                <ImageList 
+                    // variant="masonry" 
+                    cols={3} gap={12}>
+                    <Fade cascade fraction={0.1} damping={0.05} >       
                         <ImageListItem key={"tst"}>
-                            <Image src="./images/catAnimation.jpg"
+                        
+                            {/* <Image src="./images/catAnimation.jpg"
                                 alt="catAnimation"
                                 preview={{
                                     imageRender: () => (
@@ -176,31 +186,62 @@ const CreativeWorks: React.FC<{}> = () => {
                                     ),
                                     toolbarRender: () => null,
                                 }}
-                            />
+                            /> */}
+                            <YouTube videoId="kov8PXBQOxk"/>
                             <ImageListItemBar position="below"
-                             title={"watch video"} 
+                            //  title={"watch video"} 
                             //  subtitle={"used as the header for this site!"} 
                         />
                         </ImageListItem>
+                    </Fade>
                 </ImageList>
                 <br />
-                <SectionTitle>marble animation</SectionTitle>
-                <ImageList variant="masonry" cols={3} gap={12}>
+                <Fade cascade fraction={0.1} damping={0.05} >
+                    <SectionTitle>marble animation</SectionTitle>
+                </Fade>
+                <ImageList 
+                    // variant="masonry" 
+                    cols={3} gap={12}>
+                <Fade cascade fraction={0.1} damping={0.05} >
                         <ImageListItem key={"tst"}>
                                 <Image src="./images/caveGirl.jpeg" alt="test"/>
                                 <ImageListItemBar position="below"
-                                title={"girl in a cave"} 
+                                title={"blue marble concept art"} 
+                                //  subtitle={"used as the header for this site!"} 
+                                />
+                        </ImageListItem>
+                        <ImageListItem key={"tst"}>
+                                <Image src="./images/caveGirl.jpeg" alt="test"/>
+                                <ImageListItemBar position="below"
+                                title={"purple marble concept art"} 
                                 //  subtitle={"used as the header for this site!"} 
                                 />
                         </ImageListItem>
                         <ImageListItem key={"tst"}>
                             <Image src="./images/caveGirl.jpeg" alt="test"/>
                             <ImageListItemBar position="below"
-                             title={"girl in a cave"} 
+                             title={"blue in chosen art style"} 
                             //  subtitle={"used as the header for this site!"} 
                             />
                         </ImageListItem>
                         <ImageListItem key={"tst"}>
+                            <Image src="./images/caveGirl.jpeg" alt="test"/>
+                            <ImageListItemBar position="below"
+                             title={"purple in chosen art style"} 
+                            //  subtitle={"used as the header for this site!"} 
+                            />
+                        </ImageListItem>
+                        {/* <ImageListItem key={"tst"}>
+                        <YouTube 
+                                        videoId="500GanMZ2O0"
+                                        style={{height:"1000"}}
+                                      />
+                            <ImageListItemBar position="below"
+                             title={"watch video"} 
+                            //  subtitle={"used as the header for this site!"} 
+                        />
+                        </ImageListItem> */}
+                        {/* <ImageListItem key={"tst"}>
                             <Image src="./images/catAnimation.jpg"
                                 alt="marbleAnimation"
                                 preview={{
@@ -217,10 +258,21 @@ const CreativeWorks: React.FC<{}> = () => {
                              title={"watch video"} 
                             //  subtitle={"used as the header for this site!"} 
                         />
-                        </ImageListItem>
+                        </ImageListItem> */}
+                        </Fade>
                     </ImageList>
-                </Paragraph>
+                    <ImageList>
+                        <Fade damping={0.05} >
+                        <ImageListItem>
+                            <YouTube videoId="500GanMZ2O0" />
+                            <ImageListItemBar position="below"
+                                title={"full video"} 
+                            />
+                        </ImageListItem>
+                        </Fade>
+                    </ImageList>
             </CreativeContent>
+            </Fade>
         </StyledCreativeWorks>
     )
 }
