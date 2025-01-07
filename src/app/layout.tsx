58cb7254ff6ba748cbb914561f00f5acc01aefc9
tc2780 <tracyc2780@gmail.com>
2025-01-07 00:34:09 -0800

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,16 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Tracy Chow",
   description: "Personal Website",
+  openGraph: {
+    title: "Tracy Chow",
+    description: "Personal Website",
+    url: "https://tc2780.netlify.app/",
+    images: [
+      {
+        url: "https://tc2780.netlify.app/images/websiteV2.png"
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
