226a05fbc726732c4c4d0b6499caea0fde18ca50
tc2780 <tracyc2780@gmail.com>
2025-01-29 18:14:27 -0800

    update tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import "./globals.css";
 import "animate.css/animate.min.css";
 
@@ -27,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-W3M7ZF33QS" />
       <body className={inter.className}>{children}</body>
     </html>
   );
